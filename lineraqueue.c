95474c7255dda272586eff90b0f78cacbe8a6f8b
Naresh-19 <patinaresh.cs24@bmsce.ac.in>
2025-11-04 09:54:01 +0530

    Add files via upload

---
+++ lineraqueue.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+#define N 5
+int queue[N];
+int front=-1;
+int rear=-1;
+void enqueue(int x){
+    if(front==rear && front==-1){
+        front=0;
+        rear=0;
+        queue[front]=x;
+    }
+    else if(rear==N-1){
+        printf("Queue is Full\n");
+    }
+    else{
+        rear++;
+        queue[rear]=x;
+    }
+}
+void dequeue(){
+    if(front==rear && front==-1){
+        printf("queue is empty\n");
+    }
+    else if(front==rear){
+        printf("the poped element is :%d \n",queue[front]);
+        front=-1;
+        rear=-1;
+    }
+    else{
+        printf("the poped element is : %d\n",queue[front]);
+        front++;
+    }
+    }
+void display(){
+
+    for(int i=front;front<=rear;front++){
+        printf("%d ",queue[front]);
+    }
+}
+int main(){
+    int p;
+    while(1){
+        char choice;
+        printf("enter the choice:a.enqueue b.dequeue c.display d.exit :\n");
+        scanf(" %c",&choice);
+        switch(choice){
+            case('a'):
+                printf("enter the number to be pushed: \n");
+                scanf(" %d",&p);
+                enqueue(p);
+                break;
+            case ('b'):
+                dequeue();
+                break;
+            case ('c'):
+                display();
+                break;
+            case ('d'):
+                printf("--exit--");
+                return 0;
+            default:
+                printf("invalid choice\n");
+                break;
+        }
+    }
+    return 0;
+}
